740ac29eeb42f5c2fdceccb2bacd0dfd26f9c953
Kaleb <kaleblub@gmail.com>
2023-07-24 00:23:05 +0300

    Added a few more variables to the settings.py

--- kaleblub_portfolio_django/settings.py
+++ kaleblub_portfolio_django/settings.py
@@ -128,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
