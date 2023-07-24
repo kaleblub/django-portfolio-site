2d2d8a24837c884ee1453d0dca828b837dcfc1a9
Kaleb <kaleblub@gmail.com>
2023-07-25 01:08:15 +0300

    Moved all of the static files to the root directory of the project. I also added a DEBUG conditional for serving static files during development.

--- kaleblub_portfolio_django/urls.py
+++ kaleblub_portfolio_django/urls.py
@@ -17,9 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 from main import views as main_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
