76040b84b2f77eacbc30da215b1cdf32e2f41793
Kaleb <kaleblub@gmail.com>
2023-10-03 00:27:09 +0300

    Trying to configure the wsgi app for vercel.

--- kaleblub_portfolio_django/wsgi.py
+++ kaleblub_portfolio_django/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kaleblub_portfolio_django.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+app = application
