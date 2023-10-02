3570f5f6b06f60fec49d376d297d87c5b17bf0e6
Kaleb <kaleblub@gmail.com>
2023-10-03 00:15:33 +0300

    Switching to DEBUG=False for Vercel

--- kaleblub_portfolio_django/settings.py
+++ kaleblub_portfolio_django/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.is-a.dev']
 
 #Email Contact Setup
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
