bfb22f45be88211e5dca1691bba08bb65beb44ac
Kaleb <kaleblub@gmail.com>
2023-10-05 23:57:24 +0300

    Fixed the static root and staticfiles dirs.

--- kaleblub_portfolio_django/settings.py
+++ kaleblub_portfolio_django/settings.py
@@ -147,8 +147,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/vercel/path0/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
