a94b45114f6d158a2b369cff2a53edac626837c9
Kaleb <kaleblub@gmail.com>
2023-10-05 23:50:10 +0300

    Trying to get the static files collected in the right spot and point to them correctly.

--- build_files.sh
+++ build_files.sh
@@ -4,5 +4,4 @@ echo "Installing pip Packages"
 pip install -r requirements.txt
 
 echo "Collecting Static Files"
-mkdir staticfiles
 python3 manage.py collectstatic --noinput
--- kaleblub_portfolio_django/settings.py
+++ kaleblub_portfolio_django/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/vercel/path0/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
 
